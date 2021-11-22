023b6cb946b9550090afdde16e62a3540212c722
kobic8 <90327224+kobic8@users.noreply.github.com>
2021-11-22 22:30:37 +0200

    Delete Youtube_dowloader directory

--- Youtube_dowloader/YouTube_downloader.py
+++
@@ -1,7 +0,0 @@
-# import numpy as np
-# import pywhatkit
-from pytube import YouTube
-link = input("Please enter the link")
-print("Downloading")
-YouTube(link).streams.filter(res="720p").first().download()
-print("Vid is successfully downloaded")
